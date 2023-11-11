0e8c656426bf4b8d42edd841b69c79aa9ebeb6e8
Dipika Mewada <deepikamewada09092000@gmail.com>
2023-11-11 06:29:40 -0800

    vite Reat
    React app created using Vite

--- namaste-dipika/src/App.js
+++ namaste-dipika/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         You have successfully created the react App
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Namaste Dipika
         </a>
       </header>
     </div>
